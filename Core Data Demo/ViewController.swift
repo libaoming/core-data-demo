837d11dea52ddd2b71a0abc466d39228a5ef4510
master <libaoming2@gmail.com>
2016-08-25 19:44:48 +0800

    add core data update \ delete

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+       // newUser.setValue("冉冉", forKey: "username")
+        //newUser.setValue("***", forKey: "password")
+       // newUser.setValue(1, forKey: "age")
+        
+        do {
+            
+           // try  context.save()
+            
+            print("+++++++++++++++++")
+            print("saved")
+            print("+++++++++++++++++")
+        } catch {
+            print("fail to store ")
+        }
+        
+        
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        //request.predicate = NSPredicate(format: "username = %@", "然然")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    
+                    if let username = result.value(forKey: "username") {
+                        
+                        print(username)
+                        
+                       //context.delete(result)
+                        
+                       // do {
+                            
+                       //     try context.save()
+                       // } catch {
+                            
+                      //  }
+                        
+                       // result.setValue("然然", forKey: "username")
+                        
+                       // do {
+                        //   try  context.save()
+                            
+                       // }catch {
+                            
+                        //}
+                        
+                    }
+                }
+
+                
+            }
+            
+            
+        } catch {
+            
+        }
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
 
 }
 
